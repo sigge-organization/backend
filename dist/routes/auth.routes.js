794529339711f313289a7c5b58f8902411ccf3ff
Jhonatas Jorge Mendonca <jhonatas30souza@gmail.com>
2026-05-31 16:58:06 -0300

    chore: clean up build artifacts and add yarn lockfile

--- dist/routes/auth.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/auth.controller.js";
-const authRoutes = Router();
-authRoutes.post("/register", authController.register);
-export default authRoutes;
