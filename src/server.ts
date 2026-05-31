bce521e5f2aa6f2813ab5a7ddc3c49fb869fac6d
Jhonatas Jorge Mendonca <jhonatas30souza@gmail.com>
2026-05-31 19:30:51 -0300

    build: add prisma generation step to build script and update server to support environment-defined ports

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
